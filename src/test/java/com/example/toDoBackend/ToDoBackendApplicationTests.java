e4822b763c4d33d17b0e16a351dab0f234b7d72c
Sebastian <93546847+harry05555@users.noreply.github.com>
2025-11-19 19:55:06 +0100

    init

---
+++ src/test/java/com/example/toDoBackend/ToDoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toDoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
