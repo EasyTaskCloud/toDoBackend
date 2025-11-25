65d61e927d798dac8492d30f8cce754efdd99555
Sebastian <93546847+harry05555@users.noreply.github.com>
2025-11-25 21:40:43 +0100

    add insert data to prod

---
+++ src/main/resources/db/migration/prod/R__Insert_sample_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO benutzer (id, vorname, nachname, bildpfad) VALUES
+(1, 'Jasmine', 'Washington', 'user-1.jpg'),
+(2, 'Emily', 'Thompson', 'user-2.jpg'),
+(3, 'Marcus', 'Johnson', 'user-3.jpg'),
+(4, 'David', 'Miller', 'user-4.jpg'),
+(5, 'Priya', 'Patel', 'user-5.jpg'),
+(6, 'Arjun', 'Singh', 'user-6.jpg');
